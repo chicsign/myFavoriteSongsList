cae2f1ba069552ff2137660c008a6624e1ba1531
Lonnie <chicsignlonnie@gmail.com>
2020-04-28 16:55:01 +0900

    feat(logic) : postId 기준으로 firebaseDB 에서 삭제하는 쿼리 추가

--- app/src/main/java/com/lonnie/chicsign/myfavoritesongs/model/Info.kt
+++ app/src/main/java/com/lonnie/chicsign/myfavoritesongs/model/Info.kt
@@ -2,6 +2,7 @@ package com.lonnie.chicsign.myfavoritesongs.model
 
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.database.*
 
 data class Info(
     var postId: String = "",
@@ -23,6 +24,21 @@ data class Info(
     var writeTime: Any = Any()
 ) {
     fun onClickListener(view: View) {
+        //TODO: 추후 다른 곳으로 이동할 예정
         Toast.makeText(view.context, "Clicked: $songTitle", Toast.LENGTH_SHORT).show()
+        val ref = FirebaseDatabase.getInstance().reference
+        val deleteQuery: Query = ref.child("Posts").orderByChild("postId").equalTo("$postId")
+
+        deleteQuery.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    snapshot.ref.removeValue()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+//                Log.e(TAG, "onCancelled", databaseError.toException())
+            }
+        })
     }
 }
